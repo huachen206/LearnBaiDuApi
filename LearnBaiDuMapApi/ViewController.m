b2e3fdf6e68af3a839d42e74e6c4e04f1ee56d51
huachenGit <huachen206@163.com>
2013-03-19 17:34:13 +0800

    当前定位，搜索半径。

--- LearnBaiDuMapApi/ViewController.m
+++ LearnBaiDuMapApi/ViewController.m
@@ -25,11 +25,9 @@
     _search.delegate = self;
     
     [_mapView setShowsUserLocation:YES];
-    NSLog(@"%@",_mapView.userLocation);
     
     
-    [_search poiSearchInCity:@"北京" withKey:@"书店" pageIndex:0];
-    
+//    [_search poiSearchInCity:@"北京" withKey:@"书店" pageIndex:0];
     
     
     
@@ -46,7 +44,31 @@
         }
     }
 }
+- (void)mapView:(BMKMapView *)mapView didUpdateUserLocation:(BMKUserLocation *)userLocation
+{
+	if (userLocation != nil) {
+		NSLog(@"%f %f", userLocation.location.coordinate.latitude, userLocation.location.coordinate.longitude);
+        
+        [_mapView setCenterCoordinate:userLocation.location.coordinate animated:NO];
+        [_search poiSearchNearBy:@"书店" center:userLocation.location.coordinate radius:5000 pageIndex:0];
+        
+        BMKCircle *circle = [BMKCircle circleWithCenterCoordinate:userLocation.location.coordinate radius:5000];
+        [_mapView addOverlay:circle];
+        
 
+	}
+	
+}
+-(BMKOverlayView*)mapView:(BMKMapView *)mapView viewForOverlay:(id<BMKOverlay>)overlay{
+    if([overlay isKindOfClass:[BMKCircle class]]){
+        BMKCircleView *circleView = [[BMKCircleView alloc] initWithOverlay:overlay];
+        circleView.fillColor = [[UIColor cyanColor] colorWithAlphaComponent:0.03];
+        circleView.strokeColor = [[UIColor blueColor] colorWithAlphaComponent:0.5];
+        circleView.lineWidth = 1.5;
+        return circleView;
+    }
+    return nil;
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
